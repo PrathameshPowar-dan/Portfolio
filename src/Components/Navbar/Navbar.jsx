8a94fcb7c573f859207971dfd23e0622e499baac
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2025-07-15 22:50:09 +0530

    # bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,24 @@
 import './Navbar.css'
+import { useEffect } from 'react';
 const Navbar = () => {
+useEffect(() => {
+  const handleClick = (e) => {
+    const href = e.target.getAttribute('href');
+    if (href && href.startsWith('#')) {
+      setTimeout(() => {
+        history.replaceState(null, null, window.location.pathname + window.location.search);
+      }, 10);
+    }
+  };
+
+  const anchorLinks = document.querySelectorAll('a[href^="#"]');
+  anchorLinks.forEach(link => link.addEventListener('click', handleClick));
+
+  return () => {
+    anchorLinks.forEach(link => link.removeEventListener('click', handleClick));
+  };
+}, []);
+  
   return (
     <nav className='h-[10vh] text-white flex justify-between items-center w-full pt-2.5 pl-3 pr-3 mb-2'>
       <p className="text-3xl md:text-4xl font-extrabold">DAN</p>
