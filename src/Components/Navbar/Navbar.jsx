5cf4797e7a9cad488fa640c9db473b465ce89f4e
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2025-09-08 18:19:01 +0530

    Update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import './Navbar.css'
 import { useEffect } from 'react';
+import Resume from './Resume.pdf'
 const Navbar = () => {
 useEffect(() => {
   const handleClick = (e) => {
@@ -23,7 +24,7 @@ useEffect(() => {
     <nav className='h-[10vh] text-white flex justify-between items-center w-full pt-2.5 pl-3 pr-3 mb-2'>
       <p className="text-3xl md:text-4xl font-extrabold">DAN</p>
       <ul className="flex justify-center items-center gap-3 text-sm md:text-base font-semibold">
-        <li><a href="/RESUME.pdf" className='relative' download>Resume</a></li>
+        <li><a href={Resume} className='relative' download>Resume</a></li>
         <li><a href="#projects" className='relative'>Projects</a></li>
         <li><a href="#contact" className='relative'>Contacts</a></li>
       </ul>
